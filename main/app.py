96808e22c1847de1b397b2df0fef6d79474f2657
MMStoyanov21 <MMStoyanov21@codingburgas.bg>
2025-01-15 17:47:11 +0200

    idk1

--- main/app.py
+++ main/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+"""from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,7 +7,29 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return render_template('beforeLogin.html')
 
-
+@app.route('/after')
+def after_login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
+    app.run()"""
+
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the before login page
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('beforeLogin.html')
+
+# Route for the after login page
+@app.route('/after')
+def after_login():
+    return render_template('afterLogin.html')
+
+if __name__ == 'main':
     app.run()
